a5f810844fff04a15b091fc62f3ba33ac04ab9b2
luchenghsu <luchenghsu@gmail.com>
2015-01-28 00:06:13 -0800

    Add function to add two numbers with LinkedList

--- Util.java
+++ Util.java
@@ -23,9 +23,49 @@ class Util {
         return new String(str).substring(0, tail); 
     }
 
-
+    /*
+       Input (3->1->5), (5->9->2)
+       Output: 8->0->8
+    */
     public static LinkedList addTwoNumbers(LinkedList l1, LinkedList l2) {
+        
+        if(l1 == null && l2 == null)
+            return null;
+
         LinkedList result = new LinkedList();
+        Node t1 = l1.header;
+        Node t2 = l2.header;
+        
+        int carry = 0; 
+        while(t1 != null || t2 != null) {
+             
+             int value = carry;
+             
+             if(t1 != null)
+                value += t1.data;
+
+             if(t2 != null)
+                value += t2.data; 
+
+             if (value >= 10) {
+                value %= 10;
+                carry = 1;
+             } else {
+                carry = 0;
+             }
+
+             Node elem = new Node(value);
+             result.append(value);
+             
+             if (t1 != null)
+               t1 = t1.next;
+             
+             if (t2 != null)
+               t2 = t2.next;
+
+               
+        }        
+
         return result;
 
     }
