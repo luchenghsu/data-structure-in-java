19c960de6b7aabe1d5a90454b69bbeb2b7524517
luchenghsu <luchenghsu@gmail.com>
2015-01-29 00:27:45 -0800

    Implementation of Stack

---
+++ Stack.java
@@ -0,0 +1,31 @@
+class Stack {
+    Node top;
+
+    public void push(int item) {
+        Node t = new Node(item);
+        t.next = top;
+        top = t;
+    }
+
+    public Node pop() {
+        if (top != null) {
+           Node item = top;
+           top = top.next;
+           return item; 
+        }
+        return null;
+    }
+
+    public String toString() {
+        String result = "\n";
+        if (top == null) return result;
+
+        while(top.next != null) {
+            result = result + Integer.toString(top.data) + "\n|\n";
+            top = top.next;
+        }
+        result += Integer.toString(top.data);
+        return result;
+    }
+    
+}
