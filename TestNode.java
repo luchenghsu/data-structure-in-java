5014551a9fdb6e94fe8e7436fd18f8d8fe5e1987
luchenghsu <luchenghsu@gmail.com>
2015-02-03 08:58:33 -0800

    Add Test Program

--- TestNode.java
+++ TestNode.java
@@ -14,6 +14,11 @@ class TestNode {
         //n.deleteNode(2);
         n.deleteDups2();
         //n.traverse();
+        
+        n.prepend(0);
+        n.insertElement(1, 4);
+        n.deleteNode(0);
+        //System.out.println("n:"+ n);
 
         //Test addTwoNumbers
         LinkedList l1 = new LinkedList();
@@ -22,12 +27,33 @@ class TestNode {
         l1.append(3);
         l1.append(1);
         l1.append(5);
-        System.out.println("L1:"+l1);
+        //System.out.println("L1:"+l1);
 
         l2.append(5);
         l2.append(9);
         l2.append(2);
-        System.out.println("L2:"+l2);
+        l2.append(1);
+        //System.out.println("L2:"+l2);
+
+        LinkedList result = Util.addTwoNumbers(l1, l2);
+        //System.out.println("result="+result);
+
+        LinkedList t1 = new LinkedList();
+        //System.out.println("t1="+t1);
+
+        Stack s = new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.pop();
+        //System.out.println("Stack s="+s);
+
+        Queue q = new Queue();
+        q.enqueue(1);
+        q.enqueue(2);
+        q.enqueue(3);
+        q.dequeue();
+        System.out.println("Queue q="+q);
 
     }
 }
